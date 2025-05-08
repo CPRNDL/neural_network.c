7e3d9a46da6dcb1047582c472cebb5c00a32881f
CPRNDL <alswogjs@gmail.com>
2025-05-08 23:29:02 +0900

    add some commentaries

--- train_neural_network.c
+++ train_neural_network.c
@@ -1,54 +1,71 @@
+/*
+프로그래밍 C반 수행평가
+주제: 28 * 28 필기체 숫자 인식 인공 신경망 구현
+30509민재헌
+*/
+
+//라이브러리 include
 #include <stdio.h>
-#define _USE_MATH_DEFINES
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
 
-#define DATA 600000
-#define INPUT 784
-#define HIDDEN 20
-#define OUTPUT 4
-#define ETA 0.1
-#define EPOCH 100
+//상수 매크로 선언: 여러 상수들을 조정하기 쉽도록
+#define DATA 600000     //데이터 수
+#define INPUT 784       //데이터 크기 -> 784 = 28 * 28
+#define HIDDEN 20       //은닉층 노드 수
+#define OUTPUT 4        //출력층 노드 수 -> 출력층은 0~1 사이의 값을 가지기 때문에 10진수 숫자 예측을 위해 4비트로 설정
+#define ETA 0.1         //학습률(η)
+#define EPOCH 100       //학습 횟수
 
-double train[DATA][INPUT];
+//전역 변수 선언
+double train[DATA][INPUT];  //학습 데이터
+double answer[DATA];        //정답 데이터(int로 선언해도 상관 없지만 출력층과의 숫자 비교를 쉽게 하기 위해 double로 선언)
 
 //활성화 함수: 시그모이드 함수
 double Sigmoid(double z){
-    return 1/(1+exp(-z));
+    return 1/(1+exp(-z));   //시그모이드 함숫값을 반환
 }
 
-//limit을 갖는 난수 생성 함수
-double RandomGeneration(int n_in) {
-    double limit = sqrt(1.0/n_in);
-    return ((double)rand()/RAND_MAX)*2*limit-limit;
+//초기 가중치, 편향 설정을 위한 난수 생성 함수: Xavier Initialization
+double RandomGeneration(int n) {
+    double limit = sqrt(1.0/n);                         //역전파 기울기 소실과 폭주를 막기 위해 노드 수가 많을수록 범위값이 줄어들도록 설정
+    return ((double)rand()/RAND_MAX)*2*limit-limit;     //rand() 함수는 0~RAND_MAX를 반환하므로 이를 RAND_MAX로 나누면 0~1 사이의 수가 됨
+                                                        //*2*limit-limit -> -limit ~ +limit 범위로 변환
 }
 
 //데이터 파일 읽기
-void LoadInputData(const char* filename){ //파일 이름이 배열의 포인터를 가리키므로 const char 포인터로 받음
-    FILE *fp = fopen(filename, "r"); //fopen으로 파일 열기 -> return 주소 값을 FILE 구조체 포인터에 넣음
-    if(fp==NULL){ //포인터가 NULL을 반환 -> 인식 못 함
-        printf("Failed to open file\n"); //파일 열기 실패 출력
-        exit(1); //비정상 종료
+void LoadInputData(const char* filename){           //파일 이름이 배열의 포인터를 가리키므로 const char 포인터로 받음
+    FILE *fp = fopen(filename, "r");                //fopen으로 파일 열기 -> return 주소 값을 FILE 구조체 포인터에 넣음
+    if(fp==NULL){                                   //포인터가 NULL을 반환 -> 인식 못 함
+        printf("Failed to open file\n");            //파일 열기 실패 출력
+        exit(1);                                    //비정상 종료
     }
-    for(int i=0; i<DATA; i++){ //데이터 수 만큼 반복
-        int temp;
-        if(fscanf(fp, "%d", &temp)!=1) //fscanf 실패시
-        for(int j=0; j<INPUT; j++){ //28*28 만큼 반복
-            int temp;
-            if(fscanf(fp, "%d", &temp)!=1){
-                printf("Error in reading file\n");
-                exit(1);
+    for(int i=0; i<DATA; i++){                      //데이터 수 만큼 반복
+        int temp;                                   //임시 변수
+        answer[i]=(double)temp;                     //정답 데이터 넣기
+        if(fscanf(fp, "%d", &temp)!=1){             //fscanf 실패시
+            printf("Error in reading file\n");      //오류 메시지 출력
+            exit(1);                                //비정상 종료
+        }
+        for(int j=0; j<INPUT; j++){                 //28*28 만큼 반복
+            if(fscanf(fp, "%d", &temp)!=1){         //fscanf 실패시
+                printf("Error in reading file\n");  //오류 메시지 출력
+                exit(1);                            //비정상 종료
             }
-            train[i][j]=(double)temp;
+            train[i][j]=(double)temp;               //여러 연산을 위해 데이터를 double로 넣기
         }
     }
-    fclose(fp);
+    fclose(fp);     //파일 닫기
 }
 
-int main(){
-    srand((unsigned int)time(NULL)); //현재 시간을 시드로 사용
 
+/*
+데이터 학습 루프 함수
+*/
+
+
+int main(){
     double hidden[HIDDEN];
     double hiddenS[HIDDEN];
     double outputS[OUTPUT];
@@ -74,6 +91,8 @@ int main(){
     int teach_indexing, data_amount=DATA/10;
 
     LoadInputData("digit_data.txt");
+
+    srand((unsigned int)time(NULL)); //현재 시간을 시드로 사용
     
     for(int i=0; i<HIDDEN; i++){
         bias1[i]=RandomGeneration(INPUT);
@@ -88,9 +107,9 @@ int main(){
         }
     }
 
-    for(int epoch=0; epoch<EPOCH; epoch++){
+    for(int epoch=0; epoch<EPOCH; epoch++){                     //학습 시작
         epoch_cost=0;
-        for(int data_size=0; data_size<DATA; data_size++){
+        for(int data_size=0; data_size<DATA; data_size++){      //순방향 연산
             teach_indexing=data_size/data_amount;
 
             //입력층 -> 은닉층 계산
